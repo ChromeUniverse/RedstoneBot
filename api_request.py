ded0caec8f461d782a1925c6bdc595edc8deebde
Lucca Rodrigues <33258966+ChromeUniverse@users.noreply.github.com>
2020-12-28 22:28:55 -0300

    Dealing with "Under Maintenance" errors

--- api_request.py
+++ api_request.py
@@ -26,7 +26,21 @@ print("\nStatus code: " + str(r_login.status_code) + '\n')
 # Requesting data to internal internal API
 r_status = session.get(api_endpoint)
 
-# decoding JSON response text
-data = json.loads(r_status.text)
-for key in data:
-    print(key + ": " + str(data[key]))
+# check if we got some nonsense HTMl or a JSON
+if r_status.text[2] == '<':
+    # very hacky solution, I know, I know :-\
+    # the third char of the HTML template for the PloudOS.com sites is always a '<'
+    # we'll use that to our advantage here
+
+    print("Tried to access API, got nonsense HTML. *sigh*")
+    print("Either RedstoneBot is broken or PloudOS is undergoing maintenance.")
+    print("Please login to PloudOS.com and visit https://ploudos.com/server for details.")
+    print("If you think this is RedstoneBot's fault, please visit github.com/ChromeUniverse/RedstoneBot and open an issue.")
+else:
+    # decoding JSON response text
+    data = json.loads(r_status.text)
+
+    print("Got the JSON data! Here it is: \n")
+
+    for key in data:
+        print(key + ": " + str(data[key]))
