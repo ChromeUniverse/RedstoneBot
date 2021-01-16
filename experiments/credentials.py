a8faa9f25c65091cc5eb522216c85289ac4075dc
Lucca <omnichrome@gmail.com>
2021-01-16 00:46:14 -0300

    Resetting passwords and bot token cause i'm stupid

--- experiments/credentials.py
+++ experiments/credentials.py
@@ -1,14 +1,10 @@
 # Replace these with your actual PloudOS credentials
 
-#username = 'username'
-#password = 'password'
-username = 'ChromeUniverse'
-password = 'mV8nxJ3jxJHCzMy'
+username = 'username'
+password = 'password'
 
 # Enter your secret bot token here!
-#token = 'bot_token_goes_here'
-token = 'NzY5NzYxMjcwMjY5NDc2ODg3.X5TuDA.TRYUSMRaDRbiOXGzdBAN5jcFx4M'
+token = 'bot_token_goes_here'
 
 # Enter your ServerID here!
-#serverID = 's00000'
-serverID = 's10434'
+serverID = 's00000'
