553c2a4d991f8c3e83b01c8b596a305cb59b4f51
Lucca <omnichrome@gmail.com>
2021-01-18 14:14:52 -0300

    Added custom help, queuetime command, server location selector

--- code/bot.py
+++ code/bot.py
@@ -7,12 +7,14 @@ from discord.ext import commands
 
 # function imports
 from login import login
+from format_help import format_help
 
 from functions import (
     get_status,
     activate,
     deactivate,
     reactivate,
+    get_times
 )
 
 # credentials import
@@ -41,6 +43,23 @@ async def on_ready():
     print('Login status code: ' + str(login(username, password, session)))
     print('Bot is ready.')
 
+
+client.remove_command('help')
+@client.command()
+async def help(ctx):
+
+    title, content = format_help()
+
+    # format and send rich embed
+    page=discord.Embed(
+        title=title,
+        description=content,
+        colour=discord.Colour.from_rgb(221,55,55)
+    )
+    await ctx.send(embed=page)
+
+
+
 # ping command - replies with "Pong!" + connection latency in miliseconds
 @client.command()
 async def ping(ctx):
@@ -53,26 +72,53 @@ async def status(ctx):
     status, title, content = await get_status(session)
 
     # format and send rich embed
-    page1=discord.Embed(
+    page=discord.Embed(
         title=title,
         description=content,
         colour=discord.Colour.from_rgb(221,55,55)
     )
-    await ctx.send(embed=page1)
+    await ctx.send(embed=page)
+
+# queueTime command- displays queue waiting times
+@client.command()
+async def queueTime(ctx):
+    # get queue times
+    title, content = await get_times(session)
+
+    # format and send rich embed
+    page=discord.Embed(
+        title=title,
+        description=content,
+        colour=discord.Colour.from_rgb(221,55,55)
+    )
+    await ctx.send(embed=page)
+
 
 looping = False
 # open command - activates the server
 @client.command()
-async def start(ctx):
-    await ctx.send('Activating server... please wait.')
+async def start(ctx, arg=None):
 
+    if arg == '1':
+        message = 'Nuremberg selected.'
+
+    elif arg == '2':
+        message = 'St. Louis selected.'
+
+    else:
+        await ctx.send('You forgot to specify a location! The syntax for this command is: ```!redstone start [location]\n[location] = 1 🠖 Nuremberg, Germany\n[location] = 2 🠖 St. Louis, USA```')
+        return None
+
+
+    await ctx.send(message + ' Activating server... please wait.')
+    return None
     global looping
     print("bot.py looping is..." + str(looping))
 
     if looping == False:
         looping = True
         # client.loop.create_task(activate(ctx, session))
-        looping = await activate(ctx, session)
+        looping = await activate(ctx, session, arg)
     else:
         await ctx.send('Activation already in progress!')
 
