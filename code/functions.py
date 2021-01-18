ddc2cf28e1cced2093e7ef410a63181ad7e057a9
Lucca <omnichrome@gmail.com>
2021-01-18 14:13:48 -0300

    Added queueTime retrieval function + server location selector

--- code/functions.py
+++ code/functions.py
@@ -1,7 +1,9 @@
 import json
 import asyncio
+from bs4 import BeautifulSoup
 
-from format import format
+from format_status import format_status
+from format_queuetime import format_queuetime
 
 from urls import (
     login_url,
@@ -42,11 +44,70 @@ async def get_status(session):
         data = json.loads(r_status.text)
         print("Got the JSON!")
 
-        status, title, content = format(data)
+        status, title, content = format_status(data)
 
         return status, title, content
 
-async def activate(ctx, session):
+async def get_times(session):
+    # Requesting data to internal internal API
+    r_times = session.get(location_url)
+    # check if we got some nonsense HTMl or a JSON
+    if r_times.text[2] == '<':
+        # very hacky solution, I know, I know :-\
+        # the third char of the HTML template for the PloudOS.com sites is always a '<'
+        # we'll use that to our advantage here
+        print("Tried to access API, got nonsense HTML. *sigh*")
+
+        return 'Something went wrong'
+
+    else:
+        # using BS4 to parse the response
+        src = r_times.content
+        soup = BeautifulSoup(src, 'html.parser')
+
+        # Nuremberg
+
+        print("Here's the nuremberg_time:")
+
+        things = soup.div.div.a.b.i.text
+
+        timestr = []
+        i = 42
+        while True:
+            newchar = things[i]
+            if newchar != ' ':
+                timestr.append(newchar)
+                i += 1
+            else:
+                break
+
+        nuremberg_time = ''.join(timestr)
+        print(nuremberg_time)
+
+        # St. Louis
+
+        print("Here's the stlouis_time:")
+
+        things = soup.div.div.i.i.i.div.div.a.text
+
+        timestr = []
+        i = 125
+        while True:
+            newchar = things[i]
+            if newchar != ' ':
+                timestr.append(newchar)
+                i += 1
+            else:
+                break
+
+        stlouis_time = ''.join(timestr)
+        print(stlouis_time)
+
+        title, content = format_queuetime(nuremberg_time, stlouis_time)
+        return title, content
+
+
+async def activate(ctx, session, arg):
 
     # stores server status from previous loop iteration
     # itialized as an invalid status (-1; see cheat sheet above)
@@ -64,7 +125,7 @@ async def activate(ctx, session):
             print("let's enter the queue")
 
             # performing GET request to queue_URl in order to enter the queue
-            r_queue = session.get(queue_url)
+            r_queue = session.get(queue_url + arg)
             print(r_queue.text)
 
             # decoding JSON response text
