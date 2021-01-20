54123c84aac33b1774bce9dc8c11ef3ddfe1d010
Lucca <omnichrome@gmail.com>
2021-01-20 13:51:56 -0300

    Update imports, add URLs w/ DB fetch, error handling,'register' function

--- code/functions.py
+++ code/functions.py
@@ -1,10 +1,13 @@
+# module imports
 import json
 import asyncio
 from bs4 import BeautifulSoup
 
+# formmating functions
 from format_status import format_status
 from format_queuetime import format_queuetime
 
+# getting urls
 from urls import (
     login_url,
     api_endpoint,
@@ -14,6 +17,15 @@ from urls import (
     start_url,
     stop_url,
 )
+
+# database functions
+from db_functions import (
+    link,
+    guild_in_db,
+    IP_in_db,
+)
+
+from serverlist import serverlist
 
 # status cheat sheet
 #
@@ -27,9 +39,13 @@ from urls import (
 # 7 -> waiting for accept
 # 8 -> preparing server reallocation
 
-async def get_status(session):
+async def get_status(session, serverID):
+    # build API endpoint URL
+    api_endpoint = 'https://ploudos.com/manage/s' + serverID + '/ajax2'
+
     # Requesting data to internal internal API
     r_status = session.get(api_endpoint)
+
     # check if we got some nonsense HTMl or a JSON
     if r_status.text[2] == '<':
         # very hacky solution, I know, I know :-\
@@ -48,9 +64,13 @@ async def get_status(session):
 
         return status, title, content
 
-async def get_times(session):
-    # Requesting data to internal internal API
+async def get_times(session, serverID):
+    # buuilding queue waiting time URL
+    location_url = 'https://ploudos.com/manage/s' + serverID + '/ajax2/location'
+
+    # Requesting data to internal  API
     r_times = session.get(location_url)
+
     # check if we got some nonsense HTMl or a JSON
     if r_times.text[2] == '<':
         # very hacky solution, I know, I know :-\
@@ -107,7 +127,7 @@ async def get_times(session):
         return title, content
 
 
-async def activate(ctx, session, arg):
+async def activate(ctx, session, serverID, arg):
 
     # stores server status from previous loop iteration
     # itialized as an invalid status (-1; see cheat sheet above)
@@ -117,15 +137,18 @@ async def activate(ctx, session, arg):
         print("new loop iteration")
 
         # getting status
-        status, title, content = await get_status(session)
+        status, title, content = await get_status(session, serverID)
 
 
         # only run activation when the server is OFFLINE
         if status == 0:
             print("let's enter the queue")
 
+            # building queue url
+            queue_url = 'https://ploudos.com/manage/s' + serverID + '/ajax2/queue/' + arg
+
             # performing GET request to queue_URl in order to enter the queue
-            r_queue = session.get(queue_url + arg)
+            r_queue = session.get(queue_url)
             print(r_queue.text)
 
             # decoding JSON response text
@@ -136,6 +159,7 @@ async def activate(ctx, session, arg):
                 message = 'Activation sucessful! Check status with `!redstone status`.'
             else:
                 message = 'Something went wrong! Please try again.'
+                await ctx.send(message)
                 break
             # sending message
             await ctx.send(message)
@@ -153,19 +177,25 @@ async def activate(ctx, session, arg):
 
         elif status == 7:
             print("let's confirm and activate the server")
+            # building accept url
+            accept_url = 'https://ploudos.com/manage/s' + serverID + '/ajax2/accept'
 
             # performing GET request to accept_url to start up
             r_accept = session.get(accept_url)
-            print(r_accept.status_code)
+            print('accept status code: ' + str(r_accept.status_code))
+            print('json response text: ' + str(r_accept.text))
 
             # decoding JSON response text
             data = json.loads(r_accept.text)
+            print('json decoded')
 
             if not data["error"]:
                 print('No errors')
-                message = 'Confirmation sucessful! Server is starting up. Check status with `!redstone status`.'
+                message = 'Confirmation successful! Server is starting up. Check status with `!redstone status`.'
             else:
+                print('oppsie poopsie!')
                 message = 'Something went wrong! Please try again.'
+                await ctx.send(message)
                 break
             # sending message
             await ctx.send(message)
@@ -183,6 +213,7 @@ async def activate(ctx, session, arg):
             await ctx.send(message)
             # breaking the loop
             return False
+            break
 
         """
 
@@ -199,13 +230,15 @@ async def activate(ctx, session, arg):
         await asyncio.sleep(2)
 
 
-async def deactivate(session):
+async def deactivate(session, serverID):
 
     # getting status
-    status, title, content = await get_status(session)
+    status, title, content = await get_status(session, serverID)
 
     # only run activation when the server is ONLINE
     if status == 2:
+        # building stop url
+        stop_url = 'https://ploudos.com/manage/s' + serverID + '/ajax2/stop'
 
         # performing GET request to accept_url to start up
         r_stop = session.get(stop_url)
@@ -227,12 +260,14 @@ async def deactivate(session):
 
     return message
 
-async def reactivate(session):
+async def reactivate(session, serverID):
     # getting status
-    status, title, content = await get_status(session)
+    status, title, content = await get_status(session, serverID)
 
     # only run activation when the server is STOPPED
     if status == 1:
+        # building restart url
+        start_url = 'https://ploudos.com/manage/s' + serverID + '/ajax2/start'
 
         # performing GET request to accept_url to start up
         r_start = session.get(start_url)
@@ -253,3 +288,47 @@ async def reactivate(session):
         message = title
 
     return message
+
+async def register(ctx, session, guildID, is_admin, setupIP):
+
+    # check if member is admin
+    if is_admin == True:
+
+        # checking for valid argument
+        if type(setupIP) == str and '.ploudos.me' in setupIP:
+
+            # check that server isn't already in DB
+            if guild_in_db(guildID) == True:
+                await ctx.send('This Discord server is already linked to a PloudOS server.')
+                return None
+            else:
+                # check that IP isn't already in DB
+                if IP_in_db(setupIP) == True:
+                    await ctx.send('This server IP is already linked to another Discord server.')
+                    return None
+                else:
+                    # Got valid argument
+                    await ctx.send("Running setup... please wait.")
+
+                    # looping through online server list to get serverID
+                    return1, return2 = await serverlist(session, setupIP)
+
+                    if return1 != False:
+                        #await ctx.send('Got valid IP!')
+                        # managed to match input IP to actual IP on server list
+                        serverID, serverName = return1, return2
+
+                        # write data to DB
+                        link(guildID, setupIP, serverID)
+
+                        await ctx.send('Setup successful! **' + serverName + '** has been linked to this Discord server.')
+
+                    else:
+                        # couldn't match user input IP and IPs on server list
+                        await ctx.send('Incorrect server IP, try again.')
+        else:
+            # invalid argument!
+            await ctx.send('Argument error!')
+    else:
+        # user doesn't have server admin
+        await ctx.send('Only users with admin permissions can use this command.')
