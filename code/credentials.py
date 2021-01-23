acbf563544b717eb0cc40a9307b7516a9aad7ecb
Lucca <omnichrome@gmail.com>
2021-01-22 22:42:59 -0300

    Removed serverID (no need for it anymore)

--- code/credentials.py
+++ code/credentials.py
@@ -1,10 +1,6 @@
 # Replace these with your actual PloudOS credentials
-
 username = 'username'
 password = 'password'
 
 # Enter your secret bot token here!
 token = 'bot_token_goes_here'
-
-# Enter your ServerID here!
-serverID = 's00000'
