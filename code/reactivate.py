46587cf1c21292f67515d1c863affb0a4b1bb5c5
Lucca <omnichrome@gmail.com>
2021-01-22 23:51:19 -0300

    Created server reactivation script

---
+++ code/reactivate.py
@@ -0,0 +1,35 @@
+# module imports
+import json
+import requests
+
+# importing status fetch function
+from get_status import get_status
+
+async def reactivate(session, serverID):
+    # getting status
+    status, title, content = await get_status(session, serverID)
+
+    # only run activation when the server is STOPPED
+    if status == 1:
+        # building restart url
+        start_url = 'https://ploudos.com/manage/s' + serverID + '/ajax2/start'
+
+        # performing GET request to accept_url to start up
+        r_start = session.get(start_url)
+        print(r_start.text)
+
+        # decoding JSON response text
+        data = json.loads(r_start.text)
+        print(data)
+
+        if not data['error']:
+            print('No errors')
+            message = 'Reactivation sucessful! Server is starting up. Check status with `!redstone status`.'
+        else:
+            message = 'Something went wrong! Please try again.'
+
+    # else, just send the title as the message
+    else:
+        message = title
+
+    return message
