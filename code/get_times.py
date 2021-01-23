8edf301ded89c6c6c4eb3f6837de5ab8d0055287
Lucca <omnichrome@gmail.com>
2021-01-22 23:50:45 -0300

    Created queue wait time fetching script

---
+++ code/get_times.py
@@ -0,0 +1,69 @@
+# module imports
+import json
+import requests
+from bs4 import BeautifulSoup
+
+# format function import
+from format_queuetime import format_queuetime
+
+async def get_times(session, serverID):
+    # buuilding queue waiting time URL
+    location_url = 'https://ploudos.com/manage/s' + serverID + '/ajax2/location'
+
+    # Requesting data to internal  API
+    r_times = session.get(location_url)
+
+    # check if we got some nonsense HTMl or a JSON
+    if r_times.text[2] == '<':
+        # very hacky solution, I know, I know :-\
+        # the third char of the HTML template for the PloudOS.com sites is always a '<'
+        # we'll use that to our advantage here
+        print("Tried to access API, got nonsense HTML. *sigh*")
+
+        return 'Something went wrong'
+
+    else:
+        # using BS4 to parse the response
+        src = r_times.content
+        soup = BeautifulSoup(src, 'html.parser')
+
+        # Nuremberg
+
+        print("Here's the nuremberg_time:")
+
+        things = soup.div.div.a.b.i.text
+
+        timestr = []
+        i = 42
+        while True:
+            newchar = things[i]
+            if newchar != ' ':
+                timestr.append(newchar)
+                i += 1
+            else:
+                break
+
+        nuremberg_time = ''.join(timestr)
+        print(nuremberg_time)
+
+        # St. Louis
+
+        print("Here's the stlouis_time:")
+
+        things = soup.div.div.i.i.i.div.div.a.text
+
+        timestr = []
+        i = 125
+        while True:
+            newchar = things[i]
+            if newchar != ' ':
+                timestr.append(newchar)
+                i += 1
+            else:
+                break
+
+        stlouis_time = ''.join(timestr)
+        print(stlouis_time)
+
+        title, content = format_queuetime(nuremberg_time, stlouis_time)
+        return title, content
