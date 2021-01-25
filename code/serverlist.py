f7b8ec7641b2e90e42091b483e336a99023047d5
Lucca <omnichrome@gmail.com>
2021-01-25 11:01:26 -0300

    aiohttp, DB function imports, check for servers in DB before API calls

--- code/serverlist.py
+++ code/serverlist.py
@@ -1,38 +1,46 @@
 # module imports
 import json
-import requests
+import aiohttp
 from bs4 import BeautifulSoup
+
+# database function imports
+from db_functions import (
+    serverID_in_db,
+    get_IP,
+)
 
 async def serverlist(session, setupIP):
     serverlist_url = 'https://ploudos.com/server/'
     # Requesting data to internal internal API
-    r_server = session.get(serverlist_url)
+    r_server = await session.get(serverlist_url)
     print('got server list')
 
     # using BS4 to parse the response
-    src = r_server.content
-    soup = BeautifulSoup(src, 'html.parser')
+    serverlist_html = await r_server.text()
+    soup = BeautifulSoup(serverlist_html, 'html.parser')
 
-    things = soup.find_all(class_="menu")
+    # list of servers on server list
+    servers = soup.find_all(class_="menu")
 
-    i = 1
+    print(servers)
+
     # looping through server entries on server list page
-    #for i in range(len(things))
-    while True:
-        try:
-            # parsing link to get serverIDs
-            link = things[i].a['href']
-            serverID = link[10:16]
-            print('trying serverID: ' + serverID)
+    for i in range(1, len(servers)):
+        # parsing link to get serverIDs
+        link = servers[i].a['href']
+        serverID = link[10:16]
+        print('trying serverID: ' + serverID)
 
-            # acessing internal API
+        # if someone hasn't registerd this server
+        if serverID_in_db(serverID) == False:
+            # get this server's IP address through API call
             api_endpoint = 'https://ploudos.com/manage/' + serverID + '/ajax2'
-            r_api = session.get(api_endpoint)
-            print('got API response ' + str(i))
-            print(r_api.text)
+            # get request to API
+            r_api = await session.get(api_endpoint)
 
-            # decoding JSON response
-            data = json.loads(r_api.text)
+            # decode JSON response
+            api_html = await r_api.text()
+            data = json.loads(api_html)
             print(data)
 
             # getting server IP address
@@ -49,10 +57,11 @@ async def serverlist(session, setupIP):
                 print('Got it!')
                 return serverID, serverName
             else:
-                print('no dice\n\n')
+                print("Didn't work.\n\n")
+        else:
+            # found server ID in database
+            print('Server already registered.\n\n')
 
-            i += 1
-        except:
-            # couldn't find matching IP
-            print('Done')
-            return False, False
+    # couldn't find matching IP
+    print("Couldn't find IP in serverlist")
+    return False, False
