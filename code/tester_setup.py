3d9897f03c75a035ca34dc0f30913a237d01a5e8
Lucca <omnichrome2@gmail.com>
2021-06-12 23:52:12 -0300

    Created setup file for bot tester script

---
+++ code/tester_setup.py
@@ -0,0 +1,9 @@
+# Replace this with the RedstoneBot clone's ID
+target_id = '123456789012345678'
+
+# Replace this with the tester bot's auth token
+tester_token = '1234567890abcdefghijklmn.opqrst.uvwxyzabcdefghijklmnopqrstu'
+
+# Replace this with the testing channel's ID
+channel_id = '123456789012345678'
+             
