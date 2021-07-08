dc067d71b75c641bd3feb42cf3cb22acbed08b50
Lucca Rodrigues <33258966+ChromeUniverse@users.noreply.github.com>
2021-07-08 18:54:58 -0300

    Updated tester_setup

--- code/tester_setup.py
+++ code/tester_setup.py
@@ -1,8 +1,8 @@
 # Replace this with the RedstoneBot clone's ID
-target_id = '801905592334942248'               
+target_id = '862776943522611201'               
 
 # Replace this with the testing channel's ID
 channel_id = '853346992986128414'
 
-prefix = '!test'
+prefix = '!ci'
              
